6594f5d6214a5544cc8c1883ec4cf6f89dfbaafc
Lone-DO <lotussythe@yahoo.com>
2016-10-10 23:43:25 -0400

    removed Copy-Paste text and used Var Source as placeholder, Soon to be minamlized even further

--- Ember.app/assets/js/clock.js
+++ Ember.app/assets/js/clock.js
@@ -11,80 +11,81 @@ $(function () {
 	
 	setInterval(function () {
 		var img = document.getElementById('clockPhase'),
-			hour = new Date().getHours();
+			hour = new Date().getHours(),
+			source = "../assets/img/Timeline/";
 		console.log(img.src);
 
 		if (hour === 0) {
-			img.src = "../assets/img/Timeline/(0000).png";
+			img.src = source + "(0000).png";
 		}
 		if (hour === 1) {
-			img.src = "../assets/img/Timeline/(0100).png";
+			img.src = source + "(0100).png";
 		}
 		if (hour === 2) {
-			img.src = "../assets/img/Timeline/(0200).png";
+			img.src = source + "(0200).png";
 		}
 		if (hour === 3) {
-			img.src = "../assets/img/Timeline/(0300).png";
+			img.src = source + "(0300).png";
 		}
 		if (hour === 4) {
-			img.src = "../assets/img/Timeline/(0400).png";
+			img.src = source + "(0400).png";
 		}
 		if (hour === 5) {
-			img.src = "../assets/img/Timeline/(0500).png";
+			img.src = source + "(0500).png";
 		}
 		if (hour === 6) {
-			img.src = "../assets/img/Timeline/(0600).png";
+			img.src = source + "(0600).png";
 		}
 		if (hour === 7) {
-			img.src = "../assets/img/Timeline/(0700).png";
+			img.src = source + "(0700).png";
 		}
 		if (hour === 8) {
-			img.src = "../assets/img/Timeline/(0800).png";
+			img.src = source + "(0800).png";
 		}
 		if (hour === 9) {
-			img.src = "../assets/img/Timeline/(0900).png";
+			img.src = source + "(0900).png";
 		}
 		if (hour === 10) {
-			img.src = "../assets/img/Timeline/(1000).png";
+			img.src = source + "(1000).png";
 		}
 		if (hour === 11) {
-			img.src = "../assets/img/Timeline/(1100).png";
+			img.src = source + "(1100).png";
 		}
 		if (hour === 12) {
-			img.src = "../assets/img/Timeline/(1200).png";
+			img.src = source + "(1200).png";
 		}
 		if (hour === 13) {
-			img.src = "../assets/img/Timeline/(1300).png";
+			img.src = source + "(1300).png";
 		}
 		if (hour === 14) {
-			img.src = "../assets/img/Timeline/(1400).png";
+			img.src = source + "(1400).png";
 		}
 		if (hour === 15) {
-			img.src = "../assets/img/Timeline/(1500).png";
+			img.src = source + "(1500).png";
 		}
 		if (hour === 16) {
-			img.src = "../assets/img/Timeline/(1600).png";
+			img.src = source + "(1600).png";
 		}
 		if (hour === 17) {
-			img.src = "../assets/img/Timeline/(1700).png";
+			img.src = source + "(1700).png";
 		}
 		if (hour === 18) {
-			img.src = "../assets/img/Timeline/(1800).png";
+			img.src = source + "(1800).png";
 		}
 		if (hour === 19) {
-			img.src = "../assets/img/Timeline/(1900).png";
+			img.src = source + "(1900).png";
 		}
 		if (hour === 20) {
-			img.src = "../assets/img/Timeline/(2000).png";
+			img.src = source + "(2000).png";
 		}
 		if (hour === 21) {
-			img.src = "../assets/img/Timeline/(2100).png";
+			img.src = source + "(2100).png";
 		}
 		if (hour === 22) {
-			img.src = "../assets/img/Timeline/(2200).png";
+			img.src = source + "(2200).png";
 		}
 		if (hour === 23) {
-			img.src = "../assets/img/Timeline/(2300).png";
+			img.src = source + "(2300).png";
 		}
-	}, 60000);
+	}, 10000);
 });
