0d02351b4fbde9812d95777bddbf3f856abd699a
Lone-DO <lotussythe@yahoo.com>
2016-10-12 14:35:24 -0400

    Cleaner Script for Clock, and pending Audio Scripts

--- Ember.app/controllers/clock.js
+++ Ember.app/controllers/clock.js
@@ -1,42 +1,75 @@
-Acclock.Clock = Ember.View.extend({
-});
+//Acclock.Clock = Ember.View.extend({}); 
 
 $(function () {
 	'use strict';
+	console.log("Function is running");
 	var refresh = 1000; //1 sec interval. multiply by desired seconds.
 	
 	//timeClock refreshes time every second...
-	setInterval(function (timeClock) {
+//	setInterval(function (timeClock) {
+//		var date = new Date(),
+//			hour = date.getHours(),
+//			minutes = date.getMinutes(),
+//			seconds = date.getSeconds();
+//			if(hour < 10) {hour = "0" + hour};
+//			if(minutes < 10) {minutes = "0" + minutes};
+//			if(seconds < 10) {seconds = "0" + seconds};
+//			var currentTime = hour + ":" + minutes + ":" + seconds;
+//		$(".clock i").text(currentTime);
+//	}, refresh);
+
+
+	$(function start(timeClock) {
 		var date = new Date(),
-			hour = date.getHours().toString(),
-			minutes = date.getMinutes().toString(),
-			seconds = date.getSeconds().toString(),
-			currentTime = hour + ":" + minutes + ":" + seconds;
+			hours = date.getHours(),
+			minutes = date.getMinutes(),
+			seconds = date.getSeconds(),
+			update = setTimeout(start, refresh);
+		if (hours < 10) {hours = "0" + hours; }
+		if (minutes < 10) {minutes = "0" + minutes; }
+		if (seconds < 10) {seconds = "0" + seconds; }
+		var currentTime = hours + ":" + minutes + ":" + seconds;
 		$(".clock i").text(currentTime);
-		
-	}, refresh);
+	});
 	
 	//timePhase refreshes clock phase each hour
 	setInterval(function (timePhase) {
 		var img = document.getElementById('clockPhase'),
-			hour = new Date().getHours(),
+			hours = new Date().getHours(),
 			source = "../assets/img/Timeline/";
-		
-		if (hour < 10) {
+
+		if (hours < 10) {
 			var i = source + "(0";
-			i += hour;
+			i += hours;
 			i += "00).png";
 			img.src = i;
 		}
-		if (hour > 9) {
+		if (hours > 9) {
 			var i = source + "(";
-			i += hour;
+			i += hours;
 			i += "00).png";
 			img.src = i;
 		}
 	}, refresh * 10);
-	
-	
-	
-	
+
+//	setInterval(function (songPhase) {
+//		var iframe = document.getElementById('songPhase'),
+//			hour = new Date().getHours(),
+//			html = "http://www.youtube.com/embed/",
+//			songID = "0vrwZlwSpg0",
+//			autoplay = "?autoplay=1";
+//		
+//		function() {
+//			var i = html;
+//			i += songID;
+//			i += autoplay;
+//			iframe.src = i;
+//			console.log = i;
+//		}
+//	}, refresh * 10);
+
+
+
+
+
 }); //End of Strict Script
---
+++ Ember.app/songList.js
@@ -0,0 +1,27 @@
+var html = "http://downloads.khinsider.com/game-soundtracks/album/animal-crossing-new-leaf/0";
+var songs = [
+	"25-12-am.mp3",
+	"14-1-am.mp3",
+	"15-2-am.mp3",
+	"16-3-am.mp3",
+	"17-4-am.mp3",
+	"18-5-am.mp3",
+	"19-6-am.mp3",
+	"20-7-am.mp3",
+	"21-8-am.mp3",
+	"22-9-am.mp3",
+	"23-10-am.mp3",
+	"24-11-am.mp3",
+	"37-12-pm.mp3",
+	"26-1-pm.mp3",
+	"27-2-pm.mp3",
+	"28-3-pm.mp3",
+	"29-4-pm.mp3",
+	"30-5-pm.mp3",
+	"31-6-pm.mp3",
+	"32-7-pm.mp3",
+	"33-8-pm.mp3",
+	"34-9-pm.mp3",
+	"35-10-pm.mp3",
+	"36-11-pm.mp3"
+];
