09d9ab351ab97a2db1b25f6594574ac0197eeb76
lone-do <lotussythe@yahoo.com>
2016-10-13 20:42:27 -0400

    Perfected script overhaul

--- Ember.app/controllers/clock.js
+++ Ember.app/controllers/clock.js
@@ -3,7 +3,56 @@
 $(function () {
 	'use strict';
 	console.log("Function is running");
-	var refresh = 1000; //1 sec interval. multiply by desired seconds.
+	var refresh = 1000, //1 sec interval. multiply by desired seconds.
+        lastHr = -1,
+        lastMin = -1,
+        lastSec = -1;
+    
+    setInterval(function () {
+        var date = new Date(),
+            hours = date.getHours(),
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds(),
+            currentTime = hours + ":" + minutes + ":" + seconds;
+        
+        var img = document.getElementById('clockPhase'),
+			source = "../assets/img/Timeline/";
+
+        //Updating Seconds
+        if (seconds !== lastSec) {
+            console.log(seconds + " seconds");
+            if (seconds === 10) {
+                console.log("Change image to 10");
+            }
+            lastSec = seconds;
+        }//End Sec refresh
+        
+        //Updating Minutes
+        if (minutes !== lastMin) {
+            console.log(minutes + " minutes");
+            if (seconds === 10) {
+                console.log("Change image to 10");
+            }
+            lastMin = minutes;
+        }//End of Min refresh
+        
+        //Updating Hours
+        if (hours !== lastHr) {
+            console.log(hours + " hours");
+            if (hours === 10) {
+                console.log("Change image to 10");
+            }
+            lastHr = hours;
+        }//end hour refresh
+        
+        if (hours < 10) {hours = "0" + hours; }
+		if (minutes < 10) {minutes = "0" + minutes; }
+		if (seconds < 10) {seconds = "0" + seconds; }
+        
+		$(".clock i").text(currentTime);
+    }, 1000);
+    
+    
 	
 	//timeClock refreshes time every second...
 //	setInterval(function (timeClock) {
@@ -19,38 +68,41 @@ $(function () {
 //	}, refresh);
 
 
-	$(function start(timeClock) {
-		var date = new Date(),
-			hours = date.getHours(),
-			minutes = date.getMinutes(),
-			seconds = date.getSeconds(),
-			update = setTimeout(start, refresh);
-		if (hours < 10) {hours = "0" + hours; }
-		if (minutes < 10) {minutes = "0" + minutes; }
-		if (seconds < 10) {seconds = "0" + seconds; }
-		var currentTime = hours + ":" + minutes + ":" + seconds;
-		$(".clock i").text(currentTime);
-	});
-	
-	//timePhase refreshes clock phase each hour
-	setInterval(function (timePhase) {
-		var img = document.getElementById('clockPhase'),
-			hours = new Date().getHours(),
-			source = "../assets/img/Timeline/";
+//	$(function start(timeClock) {
+//		var date = new Date(),
+//			hours = date.getHours(),
+//			minutes = date.getMinutes(),
+//			seconds = date.getSeconds(),
+//			update = setTimeout(start, refresh);
+//		if (hours < 10) {hours = "0" + hours; }
+//		if (minutes < 10) {minutes = "0" + minutes; }
+//		if (seconds < 10) {seconds = "0" + seconds; }
+//		var currentTime = hours + ":" + minutes + ":" + seconds;
+//		$(".clock i").text(currentTime);
+//	});
 
-		if (hours < 10) {
-			var i = source + "(0";
-			i += hours;
-			i += "00).png";
-			img.src = i;
-		}
-		if (hours > 9) {
-			var i = source + "(";
-			i += hours;
-			i += "00).png";
-			img.src = i;
-		}
-	}, refresh * 10);
+	
+//	//timePhase refreshes clock phase each hour
+//	setInterval(function (timePhase) {
+//		var img = document.getElementById('clockPhase'),
+//			hours = new Date().getHours(),
+//			source = "../assets/img/Timeline/";
+//
+//		if (hours < 10) {
+//			var i = source + "(0";
+//			i += hours;
+//			i += "00).png";
+//			img.src = i;
+//		}
+//		if (hours > 9) {
+//			var i = source + "(";
+//			i += hours;
+//			i += "00).png";
+//			img.src = i;
+//		}
+//	}, refresh * 10);
+    
+    
 
 //	setInterval(function (songPhase) {
 //		var iframe = document.getElementById('songPhase'),
