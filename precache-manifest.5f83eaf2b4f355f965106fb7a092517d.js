33b09dd4e91318b3e74c3b83165052adbbab5b35
lone-DO <lotussythe@gmail.com>
2019-10-05 05:36:42 -0400

    Updates

---
+++ precache-manifest.5f83eaf2b4f355f965106fb7a092517d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e03a2e44a4a0750bf264e55870b1b5",
+    "url": "/animalcrossing/index.html"
+  },
+  {
+    "revision": "bd87e89ea1a367699959",
+    "url": "/animalcrossing/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "c418af623cd31cfb83f9",
+    "url": "/animalcrossing/static/css/main.2c258037.chunk.css"
+  },
+  {
+    "revision": "bd87e89ea1a367699959",
+    "url": "/animalcrossing/static/js/2.c7171dd8.chunk.js"
+  },
+  {
+    "revision": "c418af623cd31cfb83f9",
+    "url": "/animalcrossing/static/js/main.c95866ba.chunk.js"
+  },
+  {
+    "revision": "b160cd9d07bcd927e649",
+    "url": "/animalcrossing/static/js/runtime~main.88235113.js"
+  },
+  {
+    "revision": "8475561c5e08ec9d7376a317d2b28d5c",
+    "url": "/animalcrossing/static/media/Animal_Crossing_Logo.8475561c.png"
+  },
+  {
+    "revision": "7e678cd8313e46230aaf0c2b209aa596",
+    "url": "/animalcrossing/static/media/FerroRosso.7e678cd8.ttf"
+  },
+  {
+    "revision": "564ec00b262da7659294e01da6fde807",
+    "url": "/animalcrossing/static/media/loading.564ec00b.gif"
+  },
+  {
+    "revision": "6839837eba7bf81fdfd32dabacefcc58",
+    "url": "/animalcrossing/static/media/weather-normal.6839837e.svg"
+  },
+  {
+    "revision": "b7e6dc1148fb62ebcfa3ad206cb59088",
+    "url": "/animalcrossing/static/media/weather-raining.b7e6dc11.svg"
+  },
+  {
+    "revision": "27113c197c7cf4579ab93239834d9c15",
+    "url": "/animalcrossing/static/media/weather-winter.27113c19.svg"
+  }
+]);
